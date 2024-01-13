e9ee197b89efd180737fc2558edd1496ba60fc4c
Hamzah Qasim <hqqasim55@gmail.com>
2024-01-13 14:59:53 +0500

    Added db connect and CRUD apis

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-import express from "express";
-import "dotenv/config";
+require("dotenv/config");
+const express = require("express");
+const bodyParser = require("body-parser");
+const db_users = require("./db/db_users");
 
 const app = express();
+const port = process.env.SERVER_PORT;
 
-// Middleware
-app.use(express.json());
+app.use(bodyParser.json());
 
-//PORT
-const PORT = process.env.PORT;
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Routes
+app.get("/", (request, response) => {
+  response.json({ info: "Hospital Management System Back-end" });
+});
+app.get("/users", db_users.getUsers);
+app.get("/users/:id", db_users.getUserById);
+app.post("/users", db_users.createUser);
+app.put("/users/:id", db_users.updateUser);
+app.delete("/users/:id", db_users.deleteUser);
+
+// Server listen
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
 });
